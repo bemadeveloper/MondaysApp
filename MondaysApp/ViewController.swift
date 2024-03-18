3a6e2f3f22ffb9d3e12b9e9d45bf809c8e510b22
bemadeveloper <akbermet1990@icloud.com>
2024-03-18 14:06:14 +0600

    Initial Commit

---
+++ MondaysApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MondaysApp
+//
+//  Created by Bema on 18/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
